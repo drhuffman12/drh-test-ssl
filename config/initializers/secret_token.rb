cb62bdcf0193a84665d93d9856ea70ec367fcb31
Daniel Huffman <drhuffman12@yahoo.com>
2011-11-26 20:59:44 -0500

    new RoR app with dummy 'Foo' scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DrhTestSsl::Application.config.secret_token = '25964d4597801292fcfd366593b3e50c06a7205d8892d4ceceb1f1e529a1320191ffc4ba8ea679a78c4aa3d12d1bdb5877f131c7243e8d11642a514cdeff8250'
