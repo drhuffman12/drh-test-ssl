5c7d375ed6004b8e18c0e35b29993cef9e5d5831
Daniel Huffman <drhuffman12@yahoo.com>
2011-11-26 23:42:14 -0500

    force ssl for prod

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :force_app_ssl
+
+  def force_app_ssl
+    if (Rails.env == 'production')
+      if (!request.ssl?)
+        redirect_to "https://" + request.host + request.request_uri
+        flash.keep
+      end
+    end
+    return false
+  end
 end
